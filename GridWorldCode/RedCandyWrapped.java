61f7a812af87bb428b4ef67a065c203b87416328
apoon10 <apoon10@stuy.edu>
2020-06-04 14:01:44 -0400

    Made Wrapped Candies

---
+++ GridWorldCode/RedCandyWrapped.java
@@ -0,0 +1,13 @@
+import info.gridworld.actor.*;
+import java.util.ArrayList;
+import info.gridworld.grid.*;
+import java.awt.Color;
+public class RedCandy extends Candy
+{
+  public RedCandy()
+  {
+    super();
+    candynumber = 5;
+    setColor(Color.RED);
+  }
+}
