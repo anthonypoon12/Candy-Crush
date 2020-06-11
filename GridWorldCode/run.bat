559dac418580c415f242d9dd6ac66b1fa61cae90
apoon10 <apoon10@stuy.edu>
2020-06-10 23:26:28 -0400

    Compiling

--- GridWorldCode/run.bat
+++ GridWorldCode/run.bat
@@ -1,6 +1,2 @@
-javac info/gridworld/grid/*.java
-javac info/gridworld/world/*.java
-javac info/gridworld/gui/*.java
-javac info/gridworld/actor/*.java
 javac -cp ".;./gridworld.jar" CandyCrushRunner.java
 java -cp ".;./gridworld.jar" CandyCrushRunner
